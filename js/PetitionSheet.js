f2102f8a6625f31bad6e5d5e21f655482302cfbc
Chuck Lee <chuckli0706@gmail.com>
2018-05-04 21:55:55 +0800

    Add missing javascript

---
+++ js/PetitionSheet.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function() {
+    create_data_binding();
+});
+
+function copy_input_to_span(id) {
+    $("input#" + id).keyup(function() {
+        $("span#" + id).text($(this).val());
+    })
+}
+
+function create_data_binding() {
+    copy_input_to_span("id")
+    copy_input_to_span("year")
+    copy_input_to_span("month")
+    copy_input_to_span("date")
+    copy_input_to_span("county")
+    copy_input_to_span("district")
+    copy_input_to_span("villiage")
+    copy_input_to_span("neighbor")
+    copy_input_to_span("road")
+    copy_input_to_span("section")
+    copy_input_to_span("lane")
+    copy_input_to_span("alley")
+    copy_input_to_span("number")
+    copy_input_to_span("floor")
+    copy_input_to_span("part")
+
+    $("button#export_pdf").click(export_pdf)
+}
+
+function export_pdf() {
+    html2canvas($("div#sheet"), {
+        onrendered: function(canvas) {
+            var sheet_img = canvas.toDataURL('image/png');
+            var pdf = new jsPDF('l', 'mm', 'a4');
+            pdf.addImage(sheet_img, 'PNG', 0, 0);
+            pdf.save('連署書.pdf');
+        }
+    });
+}
